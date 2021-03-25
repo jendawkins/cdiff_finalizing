4c14c363f805f54ac64af71c0ae7328f9a2ed475
jendawkins <>
2021-03-25 16:32:43 -0400

    kegg paths

---
+++ dispatcher_kegg.py
@@ -0,0 +1,119 @@
+import numpy as np
+import os
+import shutil
+import argparse
+import pickle
+import time
+import sys
+import itertools
+import pickle as pkl
+
+my_str = '''
+#!/bin/bash
+#BSUB -J pylab
+#BSUB -o fl.out
+#BSUB -e fl.err
+
+# This is a sample script with specific resource requirements for the
+# **bigmemory** queue with 64GB memory requirement and memory
+# limit settings, which are both needed for reservations of
+# more than 40GB.
+# Copy this script and then submit job as follows:
+# ---
+# cd ~/lsf
+# cp templates/bsub/example_8CPU_bigmulti_64GB.lsf .
+# bsub < example_bigmulti_8CPU_64GB.lsf
+# ---
+# Then look in the ~/lsf/output folder for the script log
+# that matches the job ID number
+
+# Please make a copy of this script for your own modifications
+
+#BSUB -q rerunnable
+#BSUB -n 12
+#BSUB -M 10000
+#BSUB -R rusage[mem=10000]
+
+# Some important variables to check (Can be removed later)
+echo '---PROCESS RESOURCE LIMITS---'
+ulimit -a
+echo '---SHARED LIBRARY PATH---'
+echo $LD_LIBRARY_PATH
+echo '---APPLICATION SEARCH PATH:---'
+echo $PATH
+echo '---LSF Parameters:---'
+printenv | grep '^LSF'
+echo '---LSB Parameters:---'
+printenv | grep '^LSB'
+echo '---LOADED MODULES:---'
+module list
+echo '---SHELL:---'
+echo $SHELL
+echo '---HOSTNAME:---'
+hostname
+echo '---GROUP MEMBERSHIP (files are created in the first group listed):---'
+groups
+echo '---DEFAULT FILE PERMISSIONS (UMASK):---'
+umask
+echo '---CURRENT WORKING DIRECTORY:---'
+pwd
+echo '---DISK SPACE QUOTA---'
+df .
+echo '---TEMPORARY SCRATCH FOLDER ($TMPDIR):---'
+echo $TMPDIR
+
+# Add your job command here
+
+cd /PHShome/jjd65/cdiff_finalizing
+
+python3 ./run_kegg.py -info {0}
+'''
+parser = argparse.ArgumentParser()
+parser.add_argument("-o", "--o", help = "outpath", type = str)
+args = parser.parse_args()
+out_path = args.o
+
+if not args.o:
+    print('Specify out dir')
+    sys.exit(1)
+
+if not os.path.isdir(out_path):
+    os.mkdir(out_path)
+
+with open('temp/cdiff_dataset/kegg_dict.pkl','rb') as f:
+    kegg_dict = pkl.load(f)
+
+path = 'temp/kegg/'
+
+pathway_dist = {}
+edges = []
+edge_dict = {}
+ix = 0
+for ic, pathways in kegg_dict.items():
+    ix+=1
+    if len(pathways) == 0:
+        continue
+    for pathway in pathways:
+        edge_dict[pathway] = []
+        fname = 'kegg_path.lsf'
+        f = open(fname, 'w')
+        f.write(my_str.format(pathway))
+        f.close()
+        os.system('bsub < {}'.format(fname))
+
+for ic, pathways in kegg_dict.items():
+    with open(path + pathway + '.pkl', 'rb') as f:
+        doc = pkl.load(f)
+
+    pattern = re.compile(r'[\s] M ?[0-9][^\s]+')
+    modules = pattern.findall(doc)
+
+    for module in modules:
+        module = module.replace(' ','')
+        
+        fname = 'kegg_module.lsf'
+        f = open(fname, 'w')
+        f.write(my_str.format(module))
+        f.close()
+        os.system('bsub < {}'.format(fname))
+    
---
+++ run_kegg.py
@@ -0,0 +1,22 @@
+from Bio.KEGG import REST
+import argparse
+import pickle as pkl
+import urllib
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-info", "--info", help="module, pathway, compound, etc to be read by KEGG API", type=str)
+    args = parser.parse_args()
+
+    # try:
+    print(args.info)
+    doc = REST.kegg_get(args.info).read()
+
+    with open('temp/kegg/' + args.info + '.pkl', 'wb') as f:
+        pkl.dump(doc, f)
+
+    import pdb; pdb.set_trace()
+    # except urllib.error.HTTPError as e:
+    #     print('error')
+    #     pass
+        
