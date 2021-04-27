63e0452d2c6015114d877b5e114c42f603be8bfd
jendawkins <jendawk@mit.edu>
2021-04-27 12:29:33 -0400

    asdf

--- dispatcher_rf.py
+++ dispatcher_rf.py
@@ -81,7 +81,7 @@ if not os.path.isdir(out_path):
 
 for seed in range(0,50):
     # for model in ['LR','RF']:
-    for input_path in ['metabs','16s','bile_acids']:
+    for input_path in ['week_one_metabs','week_one_16s','week_one_bileacids']:
         for ix in range(49):
             for ix2 in range(48):
                 fname = 'cdiff_rf.lsf'
