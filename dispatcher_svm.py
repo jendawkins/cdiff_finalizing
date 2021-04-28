c82c4af8d2e54264d024760d77cee7cb5635612b
jendawkins <jendawk@mit.edu>
2021-04-27 22:28:40 -0400

    try svm

---
+++ dispatcher_svm.py
@@ -0,0 +1,96 @@
+import numpy as np
+import os
+import shutil
+import argparse
+import pickle
+import time
+import sys
+import itertools
+
+my_str = '''
+#!/bin/bash
+#BSUB -J pylab
+#BSUB -o fl.out
+#BSUB -e fl.err
+
+# This is a sample script with specific resource requirements for the
+# **bigmemory** queue with 64GB memory requirement and memory
+# limit settings, which are both needed for reservations of
+# more than 40GB.
+# Copy this script and then submit job as follows:
+# ---
+# cd ~/lsf
+# cp templates/bsub/example_8CPU_bigmulti_64GB.lsf .
+# bsub < example_bigmulti_8CPU_64GB.lsf
+# ---
+# Then look in the ~/lsf/output folder for the script log
+# that matches the job ID number
+
+# Please make a copy of this script for your own modifications
+
+#BSUB -q rerunnable
+#BSUB -n 12
+
+# Some important variables to check (Can be removed later)
+echo '---PROCESS RESOURCE LIMITS---'
+ulimit -a
+echo '---SHARED LIBRARY PATH---'
+echo $LD_LIBRARY_PATH
+echo '---APPLICATION SEARCH PATH:---'
+echo $PATH
+echo '---LSF Parameters:---'
+printenv | grep '^LSF'
+echo '---LSB Parameters:---'
+printenv | grep '^LSB'
+echo '---LOADED MODULES:---'
+module list
+echo '---SHELL:---'
+echo $SHELL
+echo '---HOSTNAME:---'
+hostname
+echo '---GROUP MEMBERSHIP (files are created in the first group listed):---'
+groups
+echo '---DEFAULT FILE PERMISSIONS (UMASK):---'
+umask
+echo '---CURRENT WORKING DIRECTORY:---'
+pwd
+echo '---DISK SPACE QUOTA---'
+df .
+echo '---TEMPORARY SCRATCH FOLDER ($TMPDIR):---'
+echo $TMPDIR
+
+# Add your job command here
+
+cd /PHShome/jjd65/cdiff_finalizing
+
+python3 ./svm_main.py -output {0} -input {2} -seed {3} -ix {4} 
+'''
+parser = argparse.ArgumentParser()
+parser.add_argument("-o", "--o", help = "outpath", type = str)
+args = parser.parse_args()
+out_path = args.o
+
+if not args.o:
+    print('Specify out dir')
+    sys.exit(1)
+
+if not os.path.isdir(out_path):
+    os.mkdir(out_path)
+
+for seed in range(0,50):
+    # for model in ['LR','RF']:
+    for input_path in ['week_one_metabs','week_one_16s','week_one_ALL']:
+    # for input_path in ['week_one_ALL', 'week_one_metabs','week_one_16s','week_one_bileacids']:
+        for ix in range(49):
+            path_out = out_path + '/' +  input_path + '/'
+            if os.path.exists(path_out + "seed" +  str(seed) + "_ix" + str(ix) + '.pkl'):
+                continue
+            else:
+                fname = 'cdiff_svm.lsf'
+                f = open(fname, 'w')
+                f.write(my_str.format(out_path, input_path, seed, ix))
+                f.close()
+                os.system('bsub < {}'.format(fname))
+
+
+
---
+++ svm_main.py
@@ -0,0 +1,75 @@
+import scipy.stats as st
+from collections import Counter
+import sklearn
+import torch
+from matplotlib import cm
+import scipy
+import itertools
+from datetime import datetime
+import os
+import time
+import sys
+import pickle as pkl
+from helper import *
+import argparse
+from basic_ml import *
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-output", "--output", help='title', type=str)
+    parser.add_argument("-input","--input", type = str)
+    parser.add_argument("-seed", "--seed", type=int)
+    parser.add_argument("-ix", "--ix", type=int)
+    args = parser.parse_args()
+
+    if not args.output:
+        args.output = 'test'
+    if not args.input:
+        args.input = 'week_one_metabs'
+    if not args.seed:
+        args.seed = 0
+    if not args.ix:
+        args.ix = 0
+
+    model = sklearn.svm.SVC(class_weight='balanced', random_state = args.seed, probability = True)
+
+    mb = basic_ml()
+    if not args.output:
+        print('Specify out dir')
+        sys.exit(1)
+
+    if not os.path.isdir(args.output):
+        os.mkdir(args.output)
+    path_out = args.output + '/' +  args.input + '/'
+    if not os.path.isdir(path_out):
+        os.mkdir(path_out)
+
+    lv = 'C'
+    feature_grid = np.logspace(-3, 3, 100)
+
+    path = 'inputs/in/' + args.input + '/'
+    with open(path + 'x.pkl', 'rb') as f:
+        x = pkl.load(f)
+    with open(path + 'y.pkl', 'rb') as f:
+        y = pkl.load(f)
+
+    if isinstance(y[0], str):
+        y = (np.array(y) == 'Recur').astype('float')
+    else:
+        y = np.array(y)
+
+    ixs = leave_one_out_cv(x, y)
+    train_index, test_index = ixs[args.ix]
+    X_train, X_test = x.iloc[train_index, :], x.iloc[test_index, :]
+    y_train, y_test = y[train_index], y[test_index]
+    final_res_dict = mb.nested_cv_func(model, X_train, y_train, optim_param='auc', plot_lambdas=False, learn_var=lv, \
+                                       feature_grid=feature_grid)
+
+    with open(path_out + "seed" +  str(args.seed) + "_ix" + str(args.ix) + '.pkl', 'wb') as f:
+        pickle.dump(final_res_dict, f)
+
+    f2 = open(args.output + "/" + args.input + "_SVM_seed" + str(args.seed) +  ".txt","a")
+    f2.write('Index ' + str(args.ix) +  ', AUC= ' + str(final_res_dict['metrics']['auc']) + '\n')
+    f2.close()
