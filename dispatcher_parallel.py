ac8b5ff71d7a319d22fbb343a769615fb11651ae
jendawkins <jendawk@mit.edu>
2021-05-10 09:58:40 -0400

    bileacids

--- dispatcher_parallel.py
+++ dispatcher_parallel.py
@@ -80,7 +80,7 @@ param = 'auc_bootstrap'
 
 for seed in range(0,50):
     # for model in ['LR','RF']:
-    for input_path in ['joint']:
+    for input_path in ['bile_acids']:
     # for input_path in ['week_one_ALL', 'week_one_metabs','week_one_16s','week_one_bileacids']:
         for ix in range(49):
             path_out = args.o + '/' + input_path + '/'
