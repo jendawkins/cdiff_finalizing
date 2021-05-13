03f0d210109edfc88843fd526474091e133eae5a
jendawkins <jendawk@mit.edu>
2021-05-13 11:25:28 -0400

    asfd

--- dispatcher_svm.py
+++ dispatcher_svm.py
@@ -29,7 +29,6 @@ my_str = '''
 # Please make a copy of this script for your own modifications
 
 #BSUB -q normal
-#BSUB -n 12
 
 # Some important variables to check (Can be removed later)
 echo '---PROCESS RESOURCE LIMITS---'
