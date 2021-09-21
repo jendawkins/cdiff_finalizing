4e95927f7c4ba0a4f27fdf5836a4fa3b256e8096
jendawkins <jendawk@mit.edu>
2021-09-21 11:00:43 -0400

    fixed

--- scripts/test_dispatcher.py
+++ scripts/test_dispatcher.py
@@ -8,59 +8,6 @@ import sys
 import itertools
 
 my_str = '''
-#!/bin/bash
-#BSUB -J cdiff
-#BSUB -o output/${6}${1}${4}/cdiff-${0}-%J.out
-#BSUB -e output/${6}${1}${4}/cdiff-${0}-%J.err
-
-# This is a sample script with specific resource requirements for the
-# **bigmemory** queue with 64GB memory requirement and memory
-# limit settings, which are both needed for reservations of
-# more than 40GB.
-# Copy this script and then submit job as follows:
-# ---
-# cd ~/lsf
-# cp templates/bsub/example_8CPU_bigmulti_64GB.lsf .
-# bsub < example_bigmulti_8CPU_64GB.lsf
-# ---
-# Then look in the ~/lsf/output folder for the script log
-# that matches the job ID number
-
-# Please make a copy of this script for your own modifications
-
-#BSUB -q normal
-
-# Some important variables to check (Can be removed later)
-echo '---PROCESS RESOURCE LIMITS---'
-ulimit -a
-echo '---SHARED LIBRARY PATH---'
-echo $LD_LIBRARY_PATH
-echo '---APPLICATION SEARCH PATH:---'
-echo $PATH
-echo '---LSF Parameters:---'
-printenv | grep '^LSF'
-echo '---LSB Parameters:---'
-printenv | grep '^LSB'
-echo '---LOADED MODULES:---'
-module list
-echo '---SHELL:---'
-echo $SHELL
-echo '---HOSTNAME:---'
-hostname
-echo '---GROUP MEMBERSHIP (files are created in the first group listed):---'
-groups
-echo '---DEFAULT FILE PERMISSIONS (UMASK):---'
-umask
-echo '---CURRENT WORKING DIRECTORY:---'
-pwd
-echo '---DISK SPACE QUOTA---'
-df .
-echo '---TEMPORARY SCRATCH FOLDER ($TMPDIR):---'
-echo $TMPDIR
-
-# Add your job command here
-
-cd /PHShome/jjd65/cdiff_finalizing/scripts
 '''
 
 my_str_cox = '''python3 ./main_cox_fast.py -ix {0} -i {1} -o {2} -type {3} -week {4} -folds {5}'''
@@ -122,22 +69,5 @@ for model in args.models:
                 path_out = out_path + '/' + in_dat + '/'
                 fname = 'cdiff.lsf'
                 if not os.path.exists(path_out + 'coef' + '_ix_' + str(0) + '.pkl'):
-                    f = open(fname, 'w')
-                    f.write(my_str.format(0, in_dat, out_path, 'coef', week, use_folds, model))
-                    f.close()
-                    os.system('bsub < {}'.format(fname))
-                    time.sleep(0.01)
-                for ii in np.arange(48):
-                    if os.path.exists(path_out + 'auc' + '_ix_' + str(ii) + '.pkl'):
-                        continue
-                    else:
-                        f = open(fname, 'w')
-                        f.write(my_str.format(ii, in_dat, out_path, 'auc', week, use_folds, model))
-                        f.close()
-                        os.system('bsub < {}'.format(fname))
-                        time.sleep(0.001)
 
-
-
-
-
+                    print(my_str.format(0, in_dat, out_path, 'coef', week, use_folds, model))
